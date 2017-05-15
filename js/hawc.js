8bf02e82af5dfb1003d9a8a335b3627178350f2e
Michael H. Roberts <mhr320@gmail.com>
2017-05-15 14:00:46 -0400

    cleaning jquery and css

--- js/hawc.js
+++ js/hawc.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
+
 	$("#barb").click(function() {
+
 		$(this).fadeOut(2000); /* Intent: click barbell and fade out then fade in Username/Password form */
 		$("form").fadeIn(3000);
 	});
 
-	$(".form-style-10 h1").click(function() {
+	$(".form-style-10 h1").click(function() { /*if this h1 is clicked, instead of the image, the form will also appear*/
+
 		$("#barb").fadeOut(2000);
 		$("form").fadeIn(3000);
 	});
-	
+
 });
