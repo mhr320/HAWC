c3e6733bc14d2bf7b38232d60c885cc7a35b1a72
Michael H. Roberts <mhr320@gmail.com>
2017-05-15 13:48:47 -0400

    hid form until barbell or SignUp Now <div> is .click()

--- js/hawc.js
+++ js/hawc.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
 	$("#barb").click(function() {
 		$(this).fadeOut(2000); /* Intent: click barbell and fade out then fade in Username/Password form */
-		/*$("#usrfm").fadeIn(3000);*/
-	})
+		$("form").fadeIn(3000);
+	});
+
+	$(".form-style-10 h1").click(function() {
+		$("#barb").fadeOut(2000);
+		$("form").fadeIn(3000);
+	});
+	
 });
