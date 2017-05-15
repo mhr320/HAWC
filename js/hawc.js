d13cb306a6d241a36376131bc561f0a088abfad4
Michael H. Roberts <mhr320@gmail.com>
2017-05-15 11:19:30 -0400

    added form and css from web example

--- js/hawc.js
+++ js/hawc.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$("#barb").click(function() {
 		$(this).fadeOut(2000); /* Intent: click barbell and fade out then fade in Username/Password form */
-		$("#usrfm").fadeIn(3000);
+		/*$("#usrfm").fadeIn(3000);*/
 	})
 });
